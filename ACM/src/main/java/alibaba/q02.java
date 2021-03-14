4cde36dafd94b46144ec3688e330b8ea2af6a941
17fyang <1099640098@qq.com>
2021-03-14 16:04:56 +0800

    init commit

--- ACM/src/main/java/alibaba/q02.java
+++ ACM/src/main/java/alibaba/q02.java
@@ -6,74 +6,76 @@ import java.util.Scanner;
  * 阿里笔试第二题
  * date:2020.3.25
  * author:yf
- * 
+ *
  */
 public class q02 {
-	//为什么你们题目中示例的2 3 6不是等差数列
-	public static void main(String[] args) {
-		solution();
-	}
-	public static void solution() {
-		Scanner sc = new Scanner(System.in);
-		int line=sc.nextInt();
-		int row=sc.nextInt();
-		int questionTimes=sc.nextInt();
-		
-		int arr[][]=new int[line][row];
-		for(int i=0;i<line;i++) {
-			for(int j=0;j<row;j++) {
-				arr[i][j]=sc.nextInt();
-			}
-		}
-		
-		for(int i=0;i<line;i++) {
-			for(int j=0;j<row;j++) {
-				if(arr[i][j]==0) {
-					int result=check(arr,i,j);
-					arr[i][j]=result;
-				}
-			}
-		}
-		
-		for(int i=0;i<questionTimes;i++) {
-			int x=sc.nextInt();
-			int y=sc.nextInt();
-			if(arr[x][y]!=0) 	System.out.println(arr[x][y]);
-			else System.out.println("Unknown");
-		}
-	}
-	private static int check(int[][] arr, int i, int j) {
-		boolean done=false;
-		boolean temp=false;
-		int d=0;
-		int startLocateX=0;
-		int startLocateY=0;
-		for(int k=0;k<arr[i].length;k++) {
-			if(arr[i][k]!=0 && !temp) {
-				temp=true;
-				startLocateX=i;
-				startLocateY=k;
-			}else if(arr[i][k]!=0) {
-				d=(arr[i][k]-arr[i][startLocateY])/(k-startLocateX);
-				done=true;
-				break;
-			}
-		}
-		if(!done) {
-			for(int k=0;k<arr.length;k++) {
-				if(arr[k][j]!=0 && !temp) {
-					temp=true;
-					startLocateX=k;
-					startLocateY=j;
-				}else if(arr[i][k]!=0) {
-					d=(arr[k][j]-arr[startLocateX][j])/(k-startLocateY);
-					done=true;
-					break;
-				}
-			}
-		}
-		if(startLocateX==i)		arr[i][j]=arr[startLocateX][startLocateY]+d*(startLocateY-j);
-		if(startLocateY==j)		arr[i][j]=arr[startLocateX][startLocateY]+d*(startLocateX-i);
-		return arr[i][j];
-	}
+    //为什么你们题目中示例的2 3 6不是等差数列
+    public static void main(String[] args) {
+        solution();
+    }
+
+    public static void solution() {
+        Scanner sc = new Scanner(System.in);
+        int line = sc.nextInt();
+        int row = sc.nextInt();
+        int questionTimes = sc.nextInt();
+
+        int[][] arr = new int[line][row];
+        for (int i = 0; i < line; i++) {
+            for (int j = 0; j < row; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < line; i++) {
+            for (int j = 0; j < row; j++) {
+                if (arr[i][j] == 0) {
+                    int result = check(arr, i, j);
+                    arr[i][j] = result;
+                }
+            }
+        }
+
+        for (int i = 0; i < questionTimes; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            if (arr[x][y] != 0) System.out.println(arr[x][y]);
+            else System.out.println("Unknown");
+        }
+    }
+
+    private static int check(int[][] arr, int i, int j) {
+        boolean done = false;
+        boolean temp = false;
+        int d = 0;
+        int startLocateX = 0;
+        int startLocateY = 0;
+        for (int k = 0; k < arr[i].length; k++) {
+            if (arr[i][k] != 0 && !temp) {
+                temp = true;
+                startLocateX = i;
+                startLocateY = k;
+            } else if (arr[i][k] != 0) {
+                d = (arr[i][k] - arr[i][startLocateY]) / (k - startLocateX);
+                done = true;
+                break;
+            }
+        }
+        if (!done) {
+            for (int k = 0; k < arr.length; k++) {
+                if (arr[k][j] != 0 && !temp) {
+                    temp = true;
+                    startLocateX = k;
+                    startLocateY = j;
+                } else if (arr[i][k] != 0) {
+                    d = (arr[k][j] - arr[startLocateX][j]) / (k - startLocateY);
+                    done = true;
+                    break;
+                }
+            }
+        }
+        if (startLocateX == i) arr[i][j] = arr[startLocateX][startLocateY] + d * (startLocateY - j);
+        if (startLocateY == j) arr[i][j] = arr[startLocateX][startLocateY] + d * (startLocateX - i);
+        return arr[i][j];
+    }
 }
--- ACM/src/main/java/duxiaoman/Q01.java
+++ ACM/src/main/java/duxiaoman/Q01.java
@@ -3,60 +3,61 @@ package duxiaoman;
 import java.util.Scanner;
 
 public class Q01 {
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int a=sc.nextInt();
-		int b=sc.nextInt();
-		int c=sc.nextInt();
-		int arr[]=new int[n+1];
-		for(int i=1;i<arr.length;i++) {
-			arr[i]=sc.nextInt();
-		}
-		int money[]=new int[n+1];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int[] arr = new int[n + 1];
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int[] money = new int[n + 1];
 		
-		int result=cost(arr,money,n,a,b,c);
-		System.out.println(result);
-		sc.close();
-	}
+        int result = cost(arr, money, n, a, b, c);
+        System.out.println(result);
+        sc.close();
+    }
 
-	private static int cost(int[] arr,int money[], int n, int a, int b, int c) {
-		for(int i=arr.length-2;i>0;i--) {
-			int min=getMin(arr,money, i, a, b,c);
-			money[i]=min;
-		}
-		for(int i=arr.length-2;i>0;i--) {
-			int min=getMin2(arr,money, i, a, b,c);
-			money[i]=min;
-		}
-		for(int i=arr.length-2;i>0;i--) {
-			int min=getMin2(arr,money, i, a, b,c);
-			money[i]=min;
-		}
-		return money[1];
-	}
-	private static int getMin2(int[] arr, int[] money, int i, int a, int b, int c) {
-		int case1=a+c*(arr.length-1-arr[i]);
-		int case2=Integer.MAX_VALUE;
-		for(int j=1;j<arr.length;j++) {
-			int jump=0;
-			if(arr[i]>=j) 	jump=b*(arr[i]-j)+a;
-			else 	jump=c*(j-arr[i])+a;
-			if(jump+money[j]<case2)		case2=jump+money[j];
-		}
-		return Math.min(case1, case2);
-	}
-	
-	
-	private static int getMin(int[] arr, int[] money, int i, int a, int b, int c) {
-		int case1=a+c*(arr.length-1-arr[i]);
-		int case2=Integer.MAX_VALUE;
-		for(int j=i+1;j<arr.length;j++) {
-			int jump=0;
-			if(arr[i]>=j) 	jump=b*(arr[i]-j)+a;
-			else 	jump=c*(j-arr[i])+a;
-			if(jump+money[j]<case2)		case2=jump+money[j];
-		}
-		return Math.min(case1, case2);
-	}
+    private static int cost(int[] arr, int money[], int n, int a, int b, int c) {
+        for (int i = arr.length - 2; i > 0; i--) {
+            int min = getMin(arr, money, i, a, b, c);
+            money[i] = min;
+        }
+        for (int i = arr.length - 2; i > 0; i--) {
+            int min = getMin2(arr, money, i, a, b, c);
+            money[i] = min;
+        }
+        for (int i = arr.length - 2; i > 0; i--) {
+            int min = getMin2(arr, money, i, a, b, c);
+            money[i] = min;
+        }
+        return money[1];
+    }
+
+    private static int getMin2(int[] arr, int[] money, int i, int a, int b, int c) {
+        int case1 = a + c * (arr.length - 1 - arr[i]);
+        int case2 = Integer.MAX_VALUE;
+        for (int j = 1; j < arr.length; j++) {
+            int jump = 0;
+            if (arr[i] >= j) jump = b * (arr[i] - j) + a;
+            else jump = c * (j - arr[i]) + a;
+            if (jump + money[j] < case2) case2 = jump + money[j];
+        }
+        return Math.min(case1, case2);
+    }
+
+
+    private static int getMin(int[] arr, int[] money, int i, int a, int b, int c) {
+        int case1 = a + c * (arr.length - 1 - arr[i]);
+        int case2 = Integer.MAX_VALUE;
+        for (int j = i + 1; j < arr.length; j++) {
+            int jump = 0;
+            if (arr[i] >= j) jump = b * (arr[i] - j) + a;
+            else jump = c * (j - arr[i]) + a;
+            if (jump + money[j] < case2) case2 = jump + money[j];
+        }
+        return Math.min(case1, case2);
+    }
 }
